c49427ed0c8d5edb9307c9cd3867d1115f685b52
thibaultmorassin <56042337+thibaultmorassin@users.noreply.github.com>
2019-11-15 16:20:53 +0100

    bitalino2
    en espérant que ça marche

--- PhyPlayTest_soft/MainForm/MainForm.cs
+++ PhyPlayTest_soft/MainForm/MainForm.cs
@@ -15,45 +15,12 @@ namespace MainForm
         public MainForm()
         {
             InitializeComponent();
+            DateTime[] tab = new DateTime[60000000];
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Debug.Write(DateTime.Now.Millisecond);
         }
-
-        private bool isPaused
-        {
-            get;
-            set;
-        }
-
-        private bool isAcquiring
-        {
-            get;
-            set;
-        }
-
-        public virtual LiveSequencer liveSequencer
-        {
-            get;
-            set;
-        }
-
-        public virtual void Pause()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public virtual void Stop()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public virtual void Start()
-        {
-            throw new System.NotImplementedException();
-        }
-
     }
 }
